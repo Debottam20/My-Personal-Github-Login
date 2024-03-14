f0878ef912827994959f698544c32113932bf95f
Debottam Mukherjee <125583945+Debottam20@users.noreply.github.com>
2024-03-14 15:51:53 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,15 @@
+function check() {
+    var name = document.getElementById("name").value;
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById('password').value;
+    if (name == "Debottam Mukherjee") {
+        if (username == "Debottam20") {
+            if (email == "d.mukherjee2003.official@gmail.com") {
+                if (password == "Debottam@2003") {
+                    open("https://github.com/Debottam20");
+                }
+            }
+        }
+    }
+}
